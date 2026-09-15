1a2520fd3c67cf0c386aa7d8bb117bcbeaf5300c
bellchocha09 <bellchocha09@gmail.com>
2026-09-14 22:25:27 -0600

    Actualización

---
+++ Rudi/rudi.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -450.0
+const GRAVITY = 45
+
+
+func _physics_process(_delta: float) -> void:
+	velocity.y += GRAVITY 
+	
+	if velocity.y > 0 and not is_on_floor():
+		$Sprite2D.flip_v = true
+	else: 
+		$Sprite2D.flip_v = false
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_pressed("ui_right"):
+		$Sprite2D.flip_h=false
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		$Sprite2D.flip_h=true
+		velocity.x = -SPEED
+	else:
+		velocity.x=0
+		
+
+	move_and_slide()
+
+	
